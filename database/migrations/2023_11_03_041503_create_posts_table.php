26c7881c77f7cefa32abd96a936904b546f85bd6
sonvt <vuthanhson041995@gmail.com>
2023-11-07 09:28:21 +0700

    create databse

---
+++ database/migrations/2023_11_03_041503_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('admin_id');
+            $table->bigInteger('category_id');
+            $table->text('title');
+            $table->text('description');
+            $table->longText('content');
+            $table->tinyInteger('type'); // 1 thuong, 2 tin dac biet
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
